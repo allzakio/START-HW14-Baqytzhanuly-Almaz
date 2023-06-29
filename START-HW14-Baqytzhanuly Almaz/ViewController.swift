15805051e9fd827ae2b578fe3c8d0366da2b30f1
allz <bakhytzhanov.7@gmail.com>
2023-06-29 09:57:50 +0600

    Initial Commit

---
+++ START-HW14-Baqytzhanuly Almaz/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  START-HW14-Baqytzhanuly Almaz
+//
+//  Created by allz on 6/29/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
